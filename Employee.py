7b7c0e420438a8cd5cd87ace81f2ef3fa20790b0
Nikita <nikitakhapare53@gmail.com>
2022-03-30 20:26:28 +0530

    [Nikita]Refactor:Program to calculate Employee Wage For a Month

--- Employee.py
+++ Employee.py
@@ -1,12 +1,16 @@
 '''
 @Author: Nikita
-@Date: 2022-03-29 20: 56: 00
+@Date: 2022-03-30 20: 25: 00
 @Last Modified by: Nikita
-@Last Modified time: 2022-03-30 : 03: 00
-@Title: Calculating Employee Wage Using Case Statement
+@Last Modified time: 2022-03-30 : 20: 25:00
+@Title: Calculating Employee Wage for a Month
 '''
 
 import random
+Rate_Per_Hour=20
+Max_Working_Days=20
+Total_Emp_Hours=0
+Total_Working_Days=0
 def workingHr(check):
 
     """
@@ -19,15 +23,13 @@ Return:
 """
     switcher={ 1: 8, 2: 4, 0: 0}
     return switcher[check]
-    
-wage_per_hour = 20
-full_time = 2
-part_time = 1
 
-check = random.randint(0,2)
-empWorkingHr=workingHr(check)
+while(Total_Working_Days<Max_Working_Days):
+    Total_Working_Days+=1
+    check=random.randint(0,2)
+    working_hours=workingHr(check)
+    Total_Emp_Hours=Total_Emp_Hours+working_hours
 
-totalWage=(wage_per_hour*empWorkingHr)
-print("Employee Wage= ",totalWage)
-
+employee_salary=(Rate_Per_Hour*Total_Emp_Hours)
+print("Employee's Monthly Wage is:",employee_salary)
  
