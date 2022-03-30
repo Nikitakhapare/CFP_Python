28f1f01d72505833af3b534b51509c05054a5b30
Nikita <nikitakhapare53@gmail.com>
2022-03-30 19:55:47 +0530

    [Nikita]Add:Program to calculate DailyWage

--- Employee.py
+++ Employee.py
@@ -2,18 +2,25 @@
 @Author: Nikita
 @Date: 2022-03-29 20: 56: 00
 @Last Modified by: Nikita
-@Last Modified time: 2022-03-29 19: 21: 00
-@Title: Checking Employee is Present or Absent
+@Last Modified time: 2022-03-30 19: 55: 00
+@Title: Calculating Daily Wage
 '''
 
 import random
 
 print("Welcome to Employee Wage Computation Programs")
-
+fullTime=1
+wagePerHr=20
+employeeWage=0
 Present=1
 rand=random.randint(0,1)
 
 if rand==1:   
     print("Employee is Present")
+    empHr=8
+    employeeWage=empHr*wagePerHr
+    print("Daily Employee Wage for Employee is ",+employeeWage)
+
 else:
     print("Employee is Absent")
+    empHr=0
