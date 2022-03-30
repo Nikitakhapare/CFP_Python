9031eb813eb584c7b60832593ed55bc2e587bdba
Nikita <nikitakhapare53@gmail.com>
2022-03-30 20:09:32 +0530

    [Nikita]Refactor:Program to calculate Employee Wage using case statement

--- Employee.py
+++ Employee.py
@@ -2,30 +2,32 @@
 @Author: Nikita
 @Date: 2022-03-29 20: 56: 00
 @Last Modified by: Nikita
-@Last Modified time: 2022-03-30 19: 57: 00
-@Title: Calculating Employee Wage
+@Last Modified time: 2022-03-30 : 03: 00
+@Title: Calculating Employee Wage Using Case Statement
 '''
 
 import random
+def workingHr(check):
 
-print("Welcome to Employee Wage Computation Programs")
-fullTime=1
-partTime=2
-wagePerHr=20
-employeeWage=0
-Present=1
-rand=random.randint(0,1)
+    """
+Description:
+     Function to Switch case
+Parameter:
+      check is to get the random number of working_hours in emply_wage_presence function
+Return:
+       returning the check variable
+"""
+    switcher={ 1: 8, 2: 4, 0: 0}
+    return switcher[check]
+    
+wage_per_hour = 20
+full_time = 2
+part_time = 1
 
-if rand==Present:   
-    print("Employee is Present")
-    empchack=random.randint(0,1)
-    if empchack==fullTime:
-           empHr=8
-    elif empchack==partTime:
-            empHr=4   
-    employeeWage=empHr*wagePerHr
-    print("Daily Employee Wage for Employee is ",+employeeWage)
+check = random.randint(0,2)
+empWorkingHr=workingHr(check)
 
-else:
-    print("Employee is Absent")
-    empHr=0
+totalWage=(wage_per_hour*empWorkingHr)
+print("Employee Wage= ",totalWage)
+
+ 
